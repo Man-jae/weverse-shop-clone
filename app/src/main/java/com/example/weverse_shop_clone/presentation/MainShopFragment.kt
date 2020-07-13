98429210c0a38249d5edcc9580fb69cb2ea583c5
Minjae Kim <alswo5052@gmail.com>
2020-07-13 16:02:23 +0900

    Shop 화면 하단 최상단 이동 버튼 클릭 동작 추가

--- app/src/main/java/com/example/weverse_shop_clone/presentation/MainShopFragment.kt
+++ app/src/main/java/com/example/weverse_shop_clone/presentation/MainShopFragment.kt
@@ -3,10 +3,14 @@ package com.example.weverse_shop_clone.presentation
 import android.os.Bundle
 import android.view.View
 import com.example.weverse_shop_clone.R
+import kotlinx.android.synthetic.main.fragment_main_shop.*
 
 class MainShopFragment : BaseFragment() {
     override fun getLayoutResId(): Int = R.layout.fragment_main_shop
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        button_back_to_top.setOnClickListener {
+            scroll_view.smoothScrollTo(0, 0)
+        }
     }
 }

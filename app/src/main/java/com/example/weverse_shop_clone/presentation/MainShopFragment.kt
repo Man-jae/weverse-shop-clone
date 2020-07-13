862bb4877e9366f179e45aeea4f54485d29f581e
Minjae Kim <alswo5052@gmail.com>
2020-07-13 16:23:11 +0900

    Shop 화면 하단 사업자 정보 표시 적용
    - 클릭시, view visible 처리
    - 애니메이션 추가

--- app/src/main/java/com/example/weverse_shop_clone/presentation/MainShopFragment.kt
+++ app/src/main/java/com/example/weverse_shop_clone/presentation/MainShopFragment.kt
@@ -2,6 +2,7 @@ package com.example.weverse_shop_clone.presentation
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.example.weverse_shop_clone.R
 import kotlinx.android.synthetic.main.fragment_main_shop.*
 
@@ -9,6 +10,16 @@ class MainShopFragment : BaseFragment() {
     override fun getLayoutResId(): Int = R.layout.fragment_main_shop
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        button_company_info.setOnClickListener {
+            if (layout_company_info.isVisible) {
+                layout_company_info.visibility = View.GONE
+                view_arrow.isChecked = false
+            } else {
+                layout_company_info.visibility = View.VISIBLE
+                view_arrow.isChecked = true
+            }
+        }
+
         button_back_to_top.setOnClickListener {
             scroll_view.smoothScrollTo(0, 0)
         }

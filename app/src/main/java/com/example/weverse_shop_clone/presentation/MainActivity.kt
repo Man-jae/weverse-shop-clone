f100d8e3b5ea68377a976d17f8782b638223d895
Minjae Kim <alswo5052@gmail.com>
2020-07-13 10:50:27 +0900

    Navigation Tab 별 화면 기본 구성

--- app/src/main/java/com/example/weverse_shop_clone/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weverse_shop_clone/presentation/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.weverse_shop_clone.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.weverse_shop_clone.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation.setupWithNavController(findNavController(R.id.fragment_main))
     }
 }
---
+++ app/src/main/java/com/example/weverse_shop_clone/presentation/MainMyFragment.kt
@@ -0,0 +1,15 @@
+package com.example.weverse_shop_clone.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weverse_shop_clone.R
+
+class MainMyFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main_my, container, false)
+    }
+}
